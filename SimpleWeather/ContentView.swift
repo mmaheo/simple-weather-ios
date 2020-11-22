56243a5fdab4e3f49689f6c203f5ebd310772e28
Maxime Maheo <mmaheo@deezer.com>
2020-11-22 21:07:25 +0100

    Create project

---
+++ SimpleWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleWeather
+//
+//  Created by Maxime Maheo on 22/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Maxime Maheo on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
